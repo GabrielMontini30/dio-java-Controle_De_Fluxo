916291f2e7e0e21b9aad7ee0194b6386359f030f
GabrielMontini30 <gabrielmontini30@gmail.com>
2026-09-22 13:07:10 -0300

    add

--- src/Contador.java
+++ src/Contador.java
@@ -14,28 +14,19 @@ public class Contador {
         int parametroDois = terminal.nextInt();
 
         try {
-
             contar(parametroUm, parametroDois);
-
         } catch (ParametrosInvalidosException exception) {
-
             System.out.println(exception.getMessage());
-
         }
-
         terminal.close();
     }
 
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
 
         if (parametroUm > parametroDois) {
-            throw new ParametrosInvalidosException(
-                    "O segundo parâmetro deve ser maior que o primeiro"
-            );
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
         }
-
         int contagem = parametroDois - parametroUm;
-
         for (int i = 1; i <= contagem; i++) {
             System.out.println("Imprimindo o número " + i);
         }
